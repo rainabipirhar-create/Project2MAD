9f9e2eb800d1fe9284400a0f5bdfb5d2d5848bd8
rainabipirhar-create <rainabipirhar@gmail.com>
2026-01-09 19:32:53 +0500

    solved forgot password error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.MyProject2"
+        applicationId = "com.example.myproject2"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
